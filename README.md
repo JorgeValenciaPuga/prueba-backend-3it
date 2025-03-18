9835b7806075ddf7392f48461d1647e052d5878e
JorgeValencia <jor.valencia@hotmail.es>
2025-03-18 12:45:47 -0300

    Initial Structure
    Initial Structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# prueba-backend-3it
-prueba backend técnica para 3IT
---
+++ src/test/java/com/tresit/creacionusuarios/CreacionusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tresit.creacionusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreacionusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
